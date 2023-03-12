e5788981586f2bd02ec336f51c5d77941c9ad486
Farbkecks <98021172+Farbkecks@users.noreply.github.com>
2023-03-12 17:25:14 +0100

    Ordner struktur

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include "Board.h"
-#include "PlayerType.h"
 #include "Player.h"
+
 
 using namespace std;
 
+
 int main()
 {
-
      Board board;
-     board.show();
-
      Player player(O);
+
      while (board.getRound() != 9)
      {
          board.show();
